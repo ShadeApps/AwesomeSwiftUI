2b4da333d4de79957b0a5648831d2ace0bf389e2
Sergey Grischyov <sergey@shadeapps.com>
2019-11-27 14:33:01 +0300

    Initial Commit

---
+++ AwesomeSwiftUITests/AwesomeSwiftUITests.swift
@@ -0,0 +1,34 @@
+//
+//  AwesomeSwiftUITests.swift
+//  AwesomeSwiftUITests
+//
+//  Created by Sergey Grischyov on 27.11.2019.
+//  Copyright © 2019 Sergey Grischyov. All rights reserved.
+//
+
+import XCTest
+@testable import AwesomeSwiftUI
+
+class AwesomeSwiftUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
